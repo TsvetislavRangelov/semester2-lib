a640d2e2f7de2d765b6441e59c9940540d83dd4e
Tsvetislav Rangelov <cveti2002mnt@gmail.com>
2022-02-19 23:53:33 +0100

    Home page layout complete, still has minor bugs

--- js/index.js
+++
@@ -1,8 +0,0 @@
-function navbarToggle() {
-    var burger = document.getElementById("navbar");
-    if (burger.className === "flex-container") {
-      burger.className += " responsive";
-    } else {
-      burger.className = "flex-container";
-    }
-  }
