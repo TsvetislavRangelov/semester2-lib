6106ff874b9bc1dec8e98ff5d121790febe016cd
Tsvetislav Rangelov <cveti2002mnt@gmail.com>
2022-04-09 15:35:43 +0200

    Content page changes

--- WAD/WAD/WAD/Pages/MangaLibrary.cshtml.cs
+++ WAD/WAD/WAD/Pages/MangaLibrary.cshtml.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ClassLibrary1.Managers;
+using Models.Models;
 
 namespace WAD.Pages
 {
